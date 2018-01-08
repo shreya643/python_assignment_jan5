3597313dd765ad44dcd1f206d06fdcbc7f1c0f46
shreya643 <shreya998.ss@gmail.com>
2018-01-08 18:55:47 +0545

    prime_num completed

---
+++ prime_num.py
@@ -0,0 +1,14 @@
+#Extract prime numbers from my_list = list(range(0,100)), and write it to a text file.
+
+my_list = list(range(0,100))
+my_list=list(filter(lambda x:x%2!=0,my_list))
+
+fp = open("D:\\python class\\primes.txt","a")
+
+for num in my_list:
+    for i in range(2,num-1):
+        if (num%i == 0):
+            break
+    else:
+        fp.write("{}\t".format(str(num)))
+fp.close()
